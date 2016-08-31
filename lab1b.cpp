da2fc872ef26520a46414d8298eec681891574d3
JAGaray <jgaray0817@gmail.com>
2016-08-31 16:03:59 -0400

    Update lab1b.cpp

--- lab1b.cpp
+++ lab1b.cpp
@@ -4,7 +4,7 @@
 	Instructor: Priyanka Samanta
 	Assignment: Lab 1
 
-	This program takes an input of time from a user and outputs a message based on the time. 
+	This program takes an input of time from a user and outputs a message based on the time.
 */
 
 #include <iostream>
@@ -15,9 +15,6 @@ using namespace std;
 int main()
 {
 
-	int 	hours_Sunset,
-		mins_Sunset;
-	
 	time_t t;
 	struct tm *now;
 	t = time(0);	//get current time
@@ -25,25 +22,39 @@ int main()
 	int hour = now->tm_hour; //retrieve current hour
 	int min = now->tm_min; //retrieve current minute
 
+    int 	hours_Sunset,
+            mins_Sunset,
+            mins_Home,
+            mins_to_arrival = (mins_Home % 60),
+            hours_to_arrival = (mins_Home / 60),
+            arrival_Hour = hour + hours_to_arrival,
+            arrival_Mins = min + mins_to_arrival;
+
 	cout << "Enter the hours of today's sunset time (12-24): ";
 	cin >>  hours_Sunset;
 	cout << "Enter the minutes of today's sunset time (0-59): ";
 	cin >> mins_Sunset;
-	
+
 	cout << "The time is " << hour<< ":" << min << endl;
-	
+
 	if (12 > hour)
-	{	
+	{
 	cout << "Good morning, Priyanka" << endl;
 	}
-	else if (hour > 12 && hour < hours_Sunset) 
+	else if (hour > 12 && hour <= hours_Sunset)
+
 	{
 	cout << "Good afternoon, Priyanka" << endl;
-	}		
+	}
 	else if (hour > hours_Sunset)
 	{
 	cout << "Good evening, Priyanka" << endl;
 	}
+
+    cout << "How many minutes until you arrive at home?" << endl;
+    cin >> mins_Home;
+    cout << "You will arrive home at " << arrival_Hour << ":" << arrival_Mins << endl;
+
 
 return 0;
 }
